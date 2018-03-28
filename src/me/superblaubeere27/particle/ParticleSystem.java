14c51d0731a321b6d579ebf6d0543f39754c0c2a
ShinduHub <clemens.wasser@gmail.com>
2018-03-28 17:37:45 +0200

    More functionality.

--- src/me/superblaubeere27/particle/ParticleSystem.java
+++ src/me/superblaubeere27/particle/ParticleSystem.java
@@ -1,16 +1,27 @@
 package me.superblaubeere27.particle;
 
+import me.superblaubeere27.util.MathUtil;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.GL11;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ParticleSystem {
     private static final float SPEED = 0.1f;
-    private static final float MIN_DISTANCE = 150f;
     private List<Particle> particleList = new ArrayList<>();
+    private boolean mouse;
+    private int dist;
+
+    public ParticleSystem(int initAmount, boolean mouse, int dist) {
+
+        addParticles(initAmount);
+        this.mouse = mouse;
+        this.dist = dist;
+
+    }
 
     public void addParticles(int amount) {
         for (int i = 0; i < amount; i++) {
@@ -34,20 +45,39 @@ public class ParticleSystem {
             GL11.glVertex2f(particle.getX(), particle.getY());
             GL11.glEnd();
 
-            for (Particle particle1 : particleList) {
-                float distance = particle.getDistanceTo(particle1);
-                if (distance < MIN_DISTANCE && (particle.getDistanceTo(Mouse.getX(), Display.getHeight() - Mouse.getY()) < MIN_DISTANCE || particle1.getDistanceTo(Mouse.getX(), Display.getHeight() - Mouse.getY()) < MIN_DISTANCE)) {
-                    float alpha = Math.min(1.0f, Math.min(1.0f, 1.0f - distance / MIN_DISTANCE));
+            if (mouse) {
 
-                    GL11.glColor4f(1.0f, 1.0f, 1.0f, alpha);
-                    GL11.glLineWidth(0.5f);
-                    GL11.glBegin(GL11.GL_LINES);
+                float distance = (float) MathUtil.distance(particle.getX(), particle.getY(), Mouse.getX(), Display.getHeight() - Mouse.getY());
+                if (distance < dist) {
+                    float alpha = Math.min(1.0f, Math.min(1.0f, 1.0f - distance / dist));
+                    drawLine(particle.getX(), particle.getY(), Mouse.getX(), Display.getHeight() - Mouse.getY(), alpha);
+                }
 
-                    GL11.glVertex2f(particle.getX(), particle.getY());
-                    GL11.glVertex2f(particle1.getX(), particle1.getY());
-                    GL11.glEnd();
+            } else {
+                for (Particle particle1 : particleList) {
+                    float distance = particle.getDistanceTo(particle1);
+                    if (distance < dist
+                            && (particle.getDistanceTo(Mouse.getX(), Display.getHeight() - Mouse.getY()) < dist
+                            || particle1.getDistanceTo(Mouse.getX(), Display.getHeight() - Mouse.getY()) < dist)) {
+                        float alpha = Math.min(1.0f, Math.min(1.0f, 1.0f - distance / dist));
+                        drawLine(particle.getX(), particle.getY(), particle1.getX(), particle1.getY(), alpha);
+
+                    }
                 }
             }
         }
     }
+
+    private void drawLine(float x, float y, float x1, float y1, float alpha) {
+
+        GL11.glColor4f(1.0f, 1.0f, 1.0f, alpha);
+        GL11.glLineWidth(0.5f);
+        GL11.glBegin(GL11.GL_LINES);
+
+        GL11.glVertex2f(x, y);
+        GL11.glVertex2f(x1, y1);
+        GL11.glEnd();
+
+    }
+
 }
